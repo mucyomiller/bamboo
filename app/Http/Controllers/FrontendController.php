93aa50f733f6da09b4dc88c7f1fe053b171e5576
Mucyo F Miller <mucyomiller@gmail.com>
2016-12-28 20:55:54 +0200

    add FrontendController

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    public function index(){
+    	return view('welcome');
+    }
+    public function menu(){
+    	return view('menu');
+    }
+    public function contact(){
+    	return view('contact');
+    }
+    public function reservation(){
+    	return view('reservation');
+    }
+}
